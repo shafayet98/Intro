6dfe2960ab00356b13380541e3c15974a18d07eb
shafayet <shafayet.islam.61@gmail.com>
2024-05-23 21:56:08 +0930

    Initial Commit

---
+++ Intro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Intro
+//
+//  Created by Shafayet Ul Islam on 23/5/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Intro/IntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroApp.swift
+//  Intro
+//
+//  Created by Shafayet Ul Islam on 23/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct IntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
